1680e18f9ae9cabb0ce05598771be5707f621f0a
Dominik Widhalm <widhalm@technikum-wien.at>
2021-04-14 11:36:48 +0200

    Added 103JT thermistor sensor library

---
+++ source/_asnx_lib_/sensors/jt103.c
@@ -0,0 +1,30 @@
+/*****
+ * @brief   ASN(x) 103JT thermistor library
+ *
+ * Library to support the use of the 103JT thermistor for temperature measurements.
+ *
+ * @file    /_asnx_lib_/sensors/jt103.h
+ * @author  $Author: Dominik Widhalm $
+ * @version $Revision: 1.0 $
+ * @date    $Date: 2021/04/14 $
+ *****/
+
+/***** INCLUDES *******************************************************/
+#include "jt103.h"
+/*** STD ***/
+#include <math.h>
+
+
+/***** FUNCTIONS ******************************************************/
+/***
+ * Calculate the temperature depending on the thermistor resistance.
+ * 
+ * @param[in]   adc_value   ADC conversion result
+ * @return      Temperature in degree Celsius (°C)
+ ***/
+float JT103_get_temperature(uint16_t adc_value) {
+    /* Calculate the thermistor's resistance */
+    float R_thermistor = (float)JT103_R_BALANCE / (((float)JT103_ADC_MAX / (float)adc_value) - 1.0);
+    /* Use the beta equation to get the temperature */
+    return ((JT103_BETA * JT103_T_ROOM) / (JT103_BETA + (JT103_T_ROOM * log(R_thermistor/JT103_R_ROOM)))) - JT103_T_K2C;
+}
---
+++ source/_asnx_lib_/sensors/jt103.h
@@ -0,0 +1,36 @@
+/*****
+ * @brief   ASN(x) 103JT thermistor library
+ *
+ * Library to support the use of the 103JT thermistor for temperature measurements.
+ *
+ * @file    /_asnx_lib_/sensors/jt103.h
+ * @author  $Author: Dominik Widhalm $
+ * @version $Revision: 1.0 $
+ * @date    $Date: 2021/04/14 $
+ *****/
+
+#ifndef _ASNX_JT103_H_
+#define _ASNX_JT103_H_
+
+/***** INCLUDES *******************************************************/
+/*** STD ***/
+#include <stdint.h>
+
+
+/***** DEFINES ********************************************************/
+/* 103JT thermistor (10k@25°C) */
+#define JT103_BETA              (3435)
+#define JT103_R_ROOM            (10000UL)
+/* Resistor Network & ADC */
+#define JT103_R_BALANCE         (10000UL)
+#define JT103_ADC_MAX           (1023)
+/* Temperature Conversion & Room Temperature */
+#define JT103_T_K2C             (273.15)
+#define JT103_T_ROOM            (JT103_T_K2C + 25)
+
+
+/***** FUNCTION PROTOTYPES ********************************************/
+float jt103_get_temperature(uint16_t adc_value);
+
+
+#endif // _ASNX_JT103_H_
