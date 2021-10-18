cb22e0692087a735d40e2a1fc25557b643a880f7
Dominik Widhalm <widhalm@technikum-wien.at>
2021-10-18 11:51:00 +0200

    Implemented fault indicators as described in SENSORS'21 paper

---
+++ source/_asnx_lib_/dca/indicators.c
@@ -0,0 +1,285 @@
+/*!
+ * @brief   ASN(x) fault indicator library -- source file
+ *
+ * Implementation of the fault indicators available on the ASN(x).
+ *
+ * @file    /_asnx_lib_/dca/indicators.c
+ * @author  Dominik Widhalm
+ * @version 1.4.0
+ * @date    2021/10/18
+ */
+
+/***** INCLUDES *******************************************************/
+#include "indicators.h"
+
+
+/***** GLOBAL VARIABLES ***********************************************/
+/*! Battery voltage monitor value array */
+float x_bat_values[X_BAT_N] = {0};
+/*! Battery voltage monitor value array index (99 if array is not filled yet) */
+uint8_t x_bat_index = 99;
+/*! Active runtime monitor value array */
+uint32_t x_art_values[X_ART_N] = {0};
+/*! Active runtime monitor value array index (99 if array is not filled yet) */
+uint8_t x_art_index = 99;
+/*! Reset monitor previous value */
+float x_rst_prev = 0.0;
+
+
+/***** FUNCTIONS ******************************************************/
+/* General */
+/*!
+ * Initialize the fault indicators, that is, reset all values expect for
+ * the reset monitor fault indicator. For this, read the value from the
+ * EEPROM to have to last value before reset.
+ */
+void indicators_init(void) {
+    /* Reset stored values */
+    x_bat_reset();
+    x_art_reset();
+    x_rst_reset();
+    /* Check if X_RST value in EEPROM was written before */
+    if(eeprom_read_byte((const uint8_t *)X_RST_EEPROM) != 0xFF) {
+        /* Read the previous reset-source indicator from EEPROM */
+        x_rst_prev = eeprom_read_float((float *)X_RST_EEPROM);
+        x_rst_set(x_rst_prev);
+    }
+}
+
+/* Node temperature monitor (X_NT) */
+/*!
+ * Update the node temperature monitor fault indicator (X_NT) value.
+ * 
+ * @param[in]   t_mcu       MCU surface temperature (°C)
+ * @param[in]   t_brd       sensor node board temperature (°C)
+ * @param[in]   t_trx       radio core temperature (°C)
+ * @return      Updated temperature monitor fault indicator (X_NT) value
+ */
+float x_nt_get_update(float t_mcu, float t_brd, float t_trx) {
+    /* Calculate the mean value of the three temperatures */
+    float mu_nt = (t_mcu + t_brd + t_trx) / 3.0;
+    /* Calculate the standard deviation of the three temperatures */
+    float sigma_nt = (float)sqrt(((double)(t_mcu-mu_nt)*(double)(t_mcu-mu_nt) + \
+                                  (double)(t_brd-mu_nt)*(double)(t_brd-mu_nt) + \
+                                  (double)(t_trx-mu_nt)*(double)(t_trx-mu_nt) \
+                                 ) / 3.0);  
+    /* Return X_NT with normalized standard deviation */
+    return (sigma_nt / X_NT_MAX);
+}
+
+
+/* Supply voltage monitor (X_VS) */
+/*!
+ * Update the supply voltage monitor fault indicator (X_VS) value.
+ * 
+ * @param[in]   v_mcu       MCU supply voltage level (V)
+ * @param[in]   v_trx       radio supply voltage level (V)
+ * @return      Updated supply voltage monitor fault indicator (X_VS) value
+ */
+float x_vs_get_update(float v_mcu, float v_trx) {
+    /* Calculate and return the normalized difference between both voltages */
+    if(v_mcu > v_trx) {
+        return ((v_mcu-v_trx) / X_VS_MAX);
+    } else {
+        return ((v_trx-v_mcu) / X_VS_MAX);
+    }
+}
+
+
+/* Battery voltage monitor (X_BAT) */
+/*!
+ * Reset the battery voltage monitor fault indicator (X_BAT) value.
+ */
+void x_bat_reset(void) {
+    /* Clear array of previous values */
+    for(uint8_t i=0; i<X_BAT_N; i++) {
+        x_bat_values[i] = 0.0;
+    }
+    /* Set array index to "not used yet" */
+    x_bat_index = 99;
+}
+
+/*!
+ * Update the battery voltage monitor fault indicator (X_BAT) value.
+ * 
+ * @param[in]   v_bat       battery voltage level (V)
+ * @return      Updated battery voltage monitor fault indicator (X_BAT) value
+ */
+float x_bat_get_update(float v_bat) {
+    /* Check if value array is empty */
+    if(x_bat_index==99) {
+        /* Fill entire array with current value */
+        for(uint8_t i=0; i<X_BAT_N; i++) {
+            x_bat_values[i] = v_bat;
+        }
+        /* Set array index to 0 */
+        x_bat_index = 0;
+    } else {
+        /* Update oldest value with new one */
+        x_bat_values[x_bat_index] = v_bat;
+        /* Update array index */
+        x_bat_index = (x_bat_index+1) % X_BAT_N;
+    }
+    /* Calculate mean value */
+    double x_bat_mean = 0;
+    for(uint8_t i=0; i<X_BAT_N; i++) {
+        x_bat_mean += (double)x_bat_values[i];
+    }
+    x_bat_mean /= X_BAT_N;
+    /* Calculate standard deviation */
+    double x_bat_stddev = 0;
+    for(uint8_t i=0; i<X_BAT_N; i++) {
+        x_bat_stddev += (((double)x_bat_values[i]-x_bat_mean) * ((double)x_bat_values[i]-x_bat_mean));
+    }
+    x_bat_stddev /= X_BAT_N;
+    x_bat_stddev = sqrt(x_bat_stddev);
+    /* Return normalized X_BAT */
+    return (float)(x_bat_stddev / X_BAT_MAX);
+}
+
+
+/* Active runtime monitor (X_ART) */
+/*!
+ * Reset the active runtime monitor fault indicator (X_ART) value.
+ */
+void x_art_reset(void) {
+    /* Clear array of previous values */
+    for(uint8_t i=0; i<X_ART_N; i++) {
+        x_art_values[i] = 0;
+    }
+    /* Set array index to "not used yet" */
+    x_art_index = 99;
+}
+
+/*!
+ * Update the active runtime monitor fault indicator (X_ART) value.
+ * 
+ * @param[in]   t_art       length of the last active phase (ms)
+ * @return      Updated active runtime monitor fault indicator (X_ART) value
+ */
+float x_art_get_update(uint32_t t_art) {
+    /* Check if value array is empty */
+    if(x_art_index==99) {
+        /* Fill entire array with current value */
+        for(uint8_t i=0; i<X_ART_N; i++) {
+            x_art_values[i] = t_art;
+        }
+        /* Set array index to 0 */
+        x_art_index = 0;
+    } else {
+        /* Update oldest value with new one */
+        x_art_values[x_art_index] = t_art;
+        /* Update array index */
+        x_art_index = (x_art_index+1) % X_ART_N;
+    }
+    /* Calculate mean value */
+    double x_art_mean = 0;
+    for(uint8_t i=0; i<X_ART_N; i++) {
+        x_art_mean += (double)x_art_values[i];
+    }
+    x_art_mean /= X_ART_N;
+    /* Calculate standard deviation */
+    double x_art_stddev = 0;
+    for(uint8_t i=0; i<X_ART_N; i++) {
+        x_art_stddev += (((double)x_art_values[i]-x_art_mean) * ((double)x_art_values[i]-x_art_mean));
+    }
+    x_art_stddev /= X_ART_N;
+    x_art_stddev = sqrt(x_art_stddev);
+    /* Return normalized X_ART */
+    return (float)(x_art_stddev / X_ART_MAX);
+}
+
+
+/* Reset monitor (X_RST) */
+/*!
+ * Reset the reset monitor fault indicator (X_RST) value.
+ */
+void x_rst_reset(void) {
+    x_rst_prev = 0;
+}
+
+/*!
+ * Set the reset monitor fault indicator (X_RST) value.
+ * 
+ * @param[in]   x_rst       new value of X_RST
+ */
+void x_rst_set(float x_rst) {
+     x_rst_prev = x_rst;
+}
+
+/*!
+ * Update the reset monitor fault indicator (X_RST) value.
+ * 
+ * @param[in]   mcusr       value of the AVR's MCUSR
+ * @return      Updated reset monitor fault indicator (X_RST) value
+ */
+float x_rst_get_update(uint8_t mcusr) {
+    /* Decrease previous value of X_RST */
+    float x_rst = (x_rst_prev * X_RST_DECAY);
+    /* Add normalized MCUSR value */
+    x_rst += ((float)(mcusr & 0x0F) / X_RST_MAX);
+    /* Update reset monitor fault indicator (X_RST) value in EEPROM if value changed notably */
+    float a = (x_rst > x_rst_prev) ? x_rst : x_rst_prev;
+    float b = (x_rst > x_rst_prev) ? x_rst_prev : x_rst;
+    if((a-b) > X_RST_UPDATE) {
+        eeprom_write_float((float *)X_RST_EEPROM,x_rst);
+    }
+    /* Updated stored previous value */
+    x_rst_prev = x_rst;
+    /* Return updated value */
+    return x_rst;
+}
+
+
+/* Software incident counter (X_IC) */
+/*!
+ * Update the software incident counter fault indicator (X_IC) value.
+ * 
+ * @param[in]   incidents   number of current software incidents
+ * @return      Updated software incident counter fault indicator (X_IC) value
+ */
+float x_ic_get_update(uint8_t incidents) {
+    /* Return normalized value */
+    return ((float)incidents / X_IC_MAX);
+}
+
+
+/* ADC self-check (X_ADC) */
+/*!
+ * Update the ADC self-check fault indicator (X_ADC) value.
+ * 
+ * @param[in]   adc_value   latest ADC conversion result
+ * @return      Updated ADC self-check fault indicator (X_ADC) value
+ */
+float x_adc_get_update(uint16_t adc_value) {
+    /* Calculate and return the normalized difference between acquired and expected value */
+    if(adc_value > X_ADC_EXPECTED) {
+        return ((adc_value-X_ADC_EXPECTED) / X_ADC_MAX);
+    } else {
+        return ((X_ADC_EXPECTED-adc_value) / X_ADC_MAX);
+    }
+}
+
+
+/* USART self-check (X_USART) */
+/*!
+ * Update the USART self-check fault indicator (X_USART) value.
+ * 
+ * @param[in]   tx          pointer to the transmit buffer array
+ * @param[in]   rx          pointer to the receive buffer array
+ * @param[in]   len         length of both arrays (should be identical)
+ * @return      Updated USART self-check fault indicator (X_USART) value
+ */
+float x_usart_get_update(uint8_t* tx, uint8_t* rx, uint8_t len) {
+    /* Variable to count number of different bytes */
+    uint8_t diff = 0;
+    /* Iterate over the arrays */
+    for(uint8_t i=0; i<len; i++) {
+        /* Check if received byte matches transmit value */
+        if(tx[i] != rx[i]) {
+            diff++;
+        }
+    }
+    /* Normalize and return value */
+    return ((float)diff / X_USART_MAX);
+}
---
+++ source/_asnx_lib_/dca/indicators.h
@@ -0,0 +1,102 @@
+/*!
+ * @brief   ASN(x) fault indicator library -- header file
+ *
+ * Implementation of the fault indicators available on the ASN(x).
+ *
+ * @file    /_asnx_lib_/dca/indicators.h
+ * @author  Dominik Widhalm
+ * @version 1.4.0
+ * @date    2021/10/18
+ */
+
+#ifndef _ASNX_INDICATORS_H_
+#define _ASNX_INDICATORS_H_
+
+/***** INCLUDES *******************************************************/
+/*** STD ***/
+#include <stdint.h>
+#include <stddef.h>
+#include <math.h>
+/*** AVR ***/
+#include <avr/eeprom.h>
+#include <avr/io.h>
+/*** ASNX LIB ***/
+#include "util/diagnostics.h"
+
+
+/***** DEFINES ********************************************************/
+/* Node temperature monitor (X_NT) */
+/*! maximum value for normalization (degrees Celsius) */
+#define X_NT_MAX                            10.0
+
+/* Supply voltage monitor (X_VS) */
+/*! maximum value for normalization (volts) */
+#define X_VS_MAX                            3.3
+
+/* Battery voltage monitor (X_BAT) */
+/*! maximum value for normalization (volts) */
+#define X_BAT_MAX                           3.3
+/*! number of consecutive measurements (N) */
+#define X_BAT_N                             5
+
+/* Active runtime monitor (X_ART) */
+/*! maximum value for normalization (seconds) */
+#define X_ART_MAX                           1.0
+/*! number of consecutive measurements (N) */
+#define X_ART_N                             5
+
+/* Reset monitor (X_RST) */
+/*! maximum value for normalization */
+#define X_RST_MAX                           25.0
+/*! decay rate (damping per update) */
+#define X_RST_DECAY                         0.92
+/*! EEPROM address */
+#define X_RST_EEPROM                        0x00
+/*! EEPROM update threshold */
+#define X_RST_UPDATE                        0.05
+
+/* Software incident counter (X_IC) */
+/*! maximum value for normalization */
+#define X_IC_MAX                            10.0
+/*! threshold for maximum number of incidents in total */
+#define X_IC_TH_TOTAL                       50
+/*! threshold for maximum number of incidents per module */
+#define X_IC_TH_SINGLE                      10
+
+/* ADC self-check (X_ADC) */
+/*! maximum value for normalization */
+#define X_ADC_MAX                           25.0
+/*! expected conversion result */
+#define X_ADC_EXPECTED                      511
+
+/* USART self-check (X_USART) */
+/*! maximum value for normalization */
+#define X_USART_MAX                         5.0
+
+
+/***** FUNCTION PROTOTYPES ********************************************/
+/* General */
+void indicators_init(void);
+
+/* Node temperature monitor (X_NT) */
+float x_nt_get_update(float t_mcu, float t_brd, float t_trx);
+/* Supply voltage monitor (X_VS) */
+float x_vs_get_update(float v_mcu, float v_trx);
+/* Battery voltage monitor (X_BAT) */
+void x_bat_reset(void);
+float x_bat_get_update(float v_bat);
+/* Active runtime monitor (X_ART) */
+void x_art_reset(void);
+float x_art_get_update(uint32_t t_art);
+/* Reset monitor (X_RST) */
+void x_rst_reset(void);
+void x_rst_set(float x_rst);
+float x_rst_get_update(uint8_t mcusr);
+/* Software incident counter (X_IC) */
+float x_ic_get_update(uint8_t incidents);
+/* ADC self-check (X_ADC) */
+float x_adc_get_update(uint16_t adc_value);
+/* USART self-check (X_USART) */
+float x_usart_get_update(uint8_t* tx, uint8_t* rx, uint8_t len);
+
+#endif // _ASNX_INDICATORS_H_
