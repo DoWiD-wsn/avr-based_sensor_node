fd2c55d297d0c8181eaeb0798982e0497f5c56e0
Dominik Widhalm <widhalm@technikum-wien.at>
2021-10-18 11:51:30 +0200

    Removed fault indicator-related functionality from generic diagnostics module

--- source/_asnx_lib_/util/diagnostics.h
+++ source/_asnx_lib_/util/diagnostics.h
@@ -5,8 +5,8 @@
  *
  * @file    /_asnx_lib_/util/diagnostics.h
  * @author  Dominik Widhalm
- * @version 1.3.0
- * @date    2021/08/09
+ * @version 1.4.0
+ * @date    2021/10/18
  */
 
 #ifndef _ASNX_DIAG_H_
@@ -32,24 +32,6 @@
 /*! R_divider enable portpin index */
 #define DIAG_RDEN_GPIO                      PC2
 
-/* Reset-source decay parameters */
-/*! decay rate (damping per update) */
-#define DIAG_DECAY_RATE                     0.92
-/*! decay minimum threshold */
-#define DIAG_DECAY_MIN                      0.10
-/*! decay maximum threshold */
-#define DIAG_DECAY_MAX                      25.0
-/*! EEPROM address */
-#define DIAG_DECAY_ADDRESS                  0x00
-/*! WDT increment value */
-#define DIAG_DECAY_WDRF                     8.0
-/*! BOD increment value */
-#define DIAG_DECAY_BORF                     4.0
-/*! EXT increment value */
-#define DIAG_DECAY_EXTRF                    2.0
-/*! PWR increment value */
-#define DIAG_DECAY_PORF                     1.0
-
 
 /***** FUNCTION PROTOTYPES ********************************************/
 /* voltage-divider setup */
@@ -60,9 +42,5 @@ void diag_disable(void);
 uint16_t diag_adc_check(void);
 float diag_read_vcc(void);
 float diag_read_vbat(void);
-/* reset-source handling */
-void diag_rsource_reset(void);
-void diag_rsource_set(float value);
-float diag_rsource_update(uint8_t mcusr);
 
 #endif // _ASNX_DIAG_H_
