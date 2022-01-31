0754fd6e32c7e3bf6a5265daed1ee47a325a2923
Dominik Widhalm <widhalm@technikum-wien.at>
2022-01-31 17:20:29 +0100

    Extension to DHT timeout fix

--- source/_asnx_lib_/sensors/dht.h
+++ source/_asnx_lib_/sensors/dht.h
@@ -32,9 +32,9 @@
 #endif
 
 /*! Minimal duration between two consecutive readings [s] */
-#define DHT_TIMING_MIN_INTERVAL 2000U
+#define DHT_TIMING_MIN_INTERVAL (2000)
 /*! Time-out value [iterations] */
-#define DHT_TIMING_TIMEOUT      500U
+#define DHT_TIMING_TIMEOUT      (200)
 
 
 /***** ENUMERATION ****************************************************/
